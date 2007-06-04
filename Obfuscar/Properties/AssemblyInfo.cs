1dc610978063e1dfed9090a540b66580813aad8d
drcforbin <drcforbin@157ddde3-1d2f-0410-8c24-d75df10a52b2>
2007-06-04 15:07:44 +0000

    Tweaked the parameter comparison in NameParamSig / ParamSig to no longer be dependent on the object's hashcode, and to allow methods that specialize generic types to be matched.
    The current solution isn't the most elegant (by far), as it just allows a generic type parameter to match any type.  It should check the type definition to determine whether the type is actually an overload.

--- Obfuscar/Properties/AssemblyInfo.cs
+++ Obfuscar/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.1.0" )]
-[assembly: AssemblyFileVersion( "1.1.0" )]
+[assembly: AssemblyVersion( "1.2.0" )]
+[assembly: AssemblyFileVersion( "1.2.0" )]
