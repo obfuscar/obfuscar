ed30ba0163d4d98dae32301d58ee27551a377e9f
drcforbin <drcforbin@157ddde3-1d2f-0410-8c24-d75df10a52b2>
2008-08-27 20:46:31 +0000

    Bumped version number to 1.3.2.
    git-svn-id: http://obfuscar.googlecode.com/svn/trunk@41 157ddde3-1d2f-0410-8c24-d75df10a52b2

--- Obfuscar/Properties/AssemblyInfo.cs
+++ Obfuscar/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.3.1" )]
-[assembly: AssemblyFileVersion( "1.3.1" )]
+[assembly: AssemblyVersion( "1.3.2" )]
+[assembly: AssemblyFileVersion( "1.3.2" )]
