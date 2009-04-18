45f433c57a5d0f03031a5727b4ae09480eed228d
webbie@cablemail.de <webbie@cablemail.de@157ddde3-1d2f-0410-8c24-d75df10a52b2>
2009-04-18 15:59:34 +0000

    Added missing source file. Version is 1.3.3 now.

--- trunk/Obfuscar/Properties/AssemblyInfo.cs
+++ trunk/Obfuscar/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.3.2" )]
-[assembly: AssemblyFileVersion( "1.3.2" )]
+[assembly: AssemblyVersion( "1.3.3" )]
+[assembly: AssemblyFileVersion( "1.3.3" )]
