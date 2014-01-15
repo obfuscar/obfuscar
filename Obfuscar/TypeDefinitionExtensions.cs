5bda72db89a48210517c847340ec50a03b4a9cfb
Carlo Kok <ck@remobjects.com>
2014-01-15 14:05:10 +0100

    0: Allow Obfuscation on members too.

--- Obfuscar/TypeDefinitionExtensions.cs
+++ Obfuscar/TypeDefinitionExtensions.cs
@@ -9,7 +9,7 @@ namespace Obfuscar
 {
     internal static class TypeDefinitionExtensions
     {
-        public static bool? ObfuscationMarked(this TypeDefinition type, bool member = false)
+        public static bool? ObfuscationMarked(this ICustomAttributeProvider type, bool member = false)
         {
             var obfuscarObfuscate = typeof(ObfuscateAttribute).FullName;
             var reflectionObfuscate = typeof(System.Reflection.ObfuscationAttribute).FullName;
