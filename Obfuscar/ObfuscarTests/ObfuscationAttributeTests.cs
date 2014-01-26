66632dcb29cb8a2e77660e7daa618c74e4b8b424
Lex Li <support@lextm.com>
2014-01-26 12:39:17 +0800

    Added test cases to match ObfuscationAttribute on methods.

--- Obfuscar/ObfuscarTests/ObfuscationAttributeTests.cs
+++ Obfuscar/ObfuscarTests/ObfuscationAttributeTests.cs
@@ -46,8 +46,27 @@ namespace ObfuscarTests
 
 			TypeDefinition classBType = inAssmDef.MainModule.GetType ("TestClasses.PublicClass");
 			ObfuscatedThing classB = map.GetClass (new TypeKey (classBType));
+            var classBmethod1 = FindByName(classBType, "PublicMethod");
+            var method2 = map.GetMethod(new MethodKey(classBmethod1));
 
 			Assert.IsTrue (classB.Status == ObfuscationStatus.Renamed, "PublicClass should have been obfuscated.");
+            Assert.IsTrue(method2.Status == ObfuscationStatus.Renamed, "PublicMethod should have been obfuscated.");
+
+            TypeDefinition classCType = inAssmDef.MainModule.GetType("TestClasses.InternalClass2");
+            ObfuscatedThing classC = map.GetClass(new TypeKey(classCType));
+            var classCmethod1 = FindByName(classCType, "PublicMethod");
+            var method1 = map.GetMethod(new MethodKey(classCmethod1));
+
+            Assert.IsTrue(classC.Status == ObfuscationStatus.Renamed, "InternalClass2 should have been obfuscated.");
+            Assert.IsTrue(method1.Status == ObfuscationStatus.Skipped, "PublicMethod shouldn't have been obfuscated.");
+
+            TypeDefinition classDType = inAssmDef.MainModule.GetType("TestClasses.PublicClass2");
+            ObfuscatedThing classD = map.GetClass(new TypeKey(classDType));
+            var classDmethod1 = FindByName(classDType, "PublicMethod");
+            var method3 = map.GetMethod(new MethodKey(classDmethod1));
+
+            Assert.IsTrue(classD.Status == ObfuscationStatus.Skipped, "PublicClass2 shouldn't have been obfuscated.");
+            Assert.IsTrue(method3.Status == ObfuscationStatus.Renamed, "PublicMethod should have been obfuscated.");
 		}
 	}
 }
