34ccfaba719f076f0ec3e0d5af3bd49c343cb5db
Lex Li <support@lextm.com>
2016-01-24 14:55:21 +0800

    2.0.2 final release.

--- ThirdParty/SharedAssemblyInfo.cs
+++ ThirdParty/SharedAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.010124.24")]
+[assembly: AssemblyVersion("2.0.020124.00")]
 [assembly: AssemblyProduct("Obfuscar 2.0.2 Beta")]
 #if (!CF)
-[assembly: AssemblyFileVersion("2.0.010124.24")]
+[assembly: AssemblyFileVersion("2.0.020124.00")]
 #endif
