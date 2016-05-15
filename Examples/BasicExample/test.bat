1e7cee397ba112ae4844e0d09d78464c4dde4295
Lex Li <support@lextm.com>
2016-05-15 17:58:55 +0800

    Simplified the sample script.

--- Examples/BasicExample/test.bat
+++ Examples/BasicExample/test.bat
@@ -1,1 +1,7 @@
-%windir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe Release.proj
+CALL ..\..\pack.bat
+
+set EnableNuGetPackageRestore=true
+set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+call %MSBuildDir%\msbuild BasicExample.sln /p:Configuration=Release /p:OutDir=..\Obfuscator_Input
+
+CALL ..\..\bin\Release\Obfuscar.Console.exe obfuscar.xml
