f6c0a4f548e0e95d1c2de30ea8f09210de4b7c23
Lex Li <support@lextm.com>
2016-12-10 12:20:57 +0000

    Bumped the version to 2.2.3.

--- debug.bat
+++ debug.bat
@@ -1,4 +1,5 @@
 set EnableNuGetPackageRestore=true
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 CALL .\.nuget\nuget.exe restore obfuscar.sln
+call %MSBuildDir%\msbuild obfuscar.sln /t:clean
 call %MSBuildDir%\msbuild obfuscar.sln
