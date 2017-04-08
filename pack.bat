2d26bdf6cc78e7ae449c2f52f4c6f88aaf6a39ca
Lex Li <support@lextm.com>
2017-04-08 11:18:42 -0400

    Upgraded to MSBuild 15.

--- pack.bat
+++ pack.bat
@@ -1,5 +1,15 @@
+@echo off
+
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+  set msBuildExe="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+)
+@echo on
+
 set EnableNuGetPackageRestore=true
-set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 CALL .\.nuget\nuget.exe restore obfuscar.sln
-call %MSBuildDir%\msbuild obfuscar.sln /p:Configuration=Release
+call %msBuildExe% obfuscar.sln /p:Configuration=Release
 CALL .\.nuget\nuget.exe pack 
