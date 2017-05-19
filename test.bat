10fc7e6e8fc50399f88e66380baddf4b6b271ec7
Lex Li <support@lextm.com>
2017-05-19 18:05:45 -0400

    Tuned for AppVeyor.

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
 cd Tests\bin\Release
-"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.exe" ObfuscarTests.dll
-"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe" ObfuscarTests.dll
+"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.exe" ObfuscarTests.dll -appveyor
+"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe" ObfuscarTests.dll -appveyor
