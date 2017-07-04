dbbd6168a7666b335e8363ad3091568362be0bd1
Lex Li <support@lextm.com>
2017-07-04 12:19:18 -0400

    Fixed sample project script.

--- Examples/BasicExample/test.bat
+++ Examples/BasicExample/test.bat
@@ -1,7 +1,21 @@
-CALL ..\..\pack.bat
+echo Step 1, build obfuscar command line tool from source code.
+call ..\..\dist.pack.bat
+
+echo Step 2, build sample project.
+
+@echo off
+
+for /f "usebackq tokens=*" %%i in (`..\..\vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
+  set msBuildExe="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
+)
+@echo on
 
 set EnableNuGetPackageRestore=true
-set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-call %MSBuildDir%\msbuild BasicExample.sln /p:Configuration=Release /p:OutDir=..\Obfuscator_Input
+call %MSBuildExe% BasicExample.sln /p:Configuration=Release /p:OutDir=..\Obfuscator_Input
 
-CALL ..\..\bin\Release\Obfuscar.Console.exe obfuscar.xml
+echo Step 3, execute obfuscation.
+call ..\..\bin\Release\Obfuscar.Console.exe obfuscar.xml
