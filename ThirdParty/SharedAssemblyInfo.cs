7cd594b834cfbca08927a1d1c568aefadbdaf7fa
Lex Li <support@lextm.com>
2017-09-14 23:36:05 -0400

    Bumped the version.

--- ThirdParty/SharedAssemblyInfo.cs
+++ ThirdParty/SharedAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 // You can specify all the values or you can use the default the Revision and
 // Build Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.2.8.0")]
-[assembly: AssemblyProduct("Obfuscar 2.2.8")]
+[assembly: AssemblyVersion("2.2.9.0")]
+[assembly: AssemblyProduct("Obfuscar 2.2.9")]
 #if (!CF)
-[assembly: AssemblyFileVersion("2.2.8.0")]
+[assembly: AssemblyFileVersion("2.2.9.0")]
 #endif
