79ba599815eb4b70b7bcefdca2c6cb832d794bb3
Patrick Hofman <patrick.hofman@invantive.com>
2019-01-28 10:26:22 +0100

    Optimize performance of string replacement.

---
+++ Obfuscar/LdStrInstructionReplacement.cs
@@ -0,0 +1,17 @@
+﻿using Mono.Cecil.Cil;
+
+namespace Obfuscar
+{
+    internal class LdStrInstructionReplacement
+    {
+        public LdStrInstructionReplacement(int oldIndex, Instruction newinstruction)
+        {
+            this.OldIndex = oldIndex;
+            NewInstruction = newinstruction;
+        }
+
+        public int OldIndex { get; }
+        
+        public Instruction NewInstruction { get; }
+    }
+}
