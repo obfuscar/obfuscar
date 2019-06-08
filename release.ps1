4880f22d73a3e51b4eaf6d80a802df394914f8c7
Lex Li <support@lextm.com>
2019-06-08 10:51:15 -0400

    Updated release.ps1 to support VS2017/2019.

--- release.ps1
+++ release.ps1
@@ -1,11 +1,34 @@
-Install-Module VSSetup -Scope CurrentUser -Force
-$instance = Get-VSSetupInstance -All
-$installDir = $instance.installationPath
-$msBuild = $installDir + '\MSBuild\15.0\Bin\MSBuild.exe'
-if (![System.IO.File]::Exists($msBuild))
+$msBuild = "msbuild"
+try
 {
-    Write-Host "MSBuild doesn't exist. Exit."
-    exit 1
+    & $msBuild /version
+    Write-Host "Likely on Linux/macOS."
+}
+catch
+{
+    Write-Host "MSBuild doesn't exist. Use VSSetup instead."
+
+    Install-Module VSSetup -Scope CurrentUser -Force
+    $instance = Get-VSSetupInstance -All | Select-VSSetupInstance -Latest
+    $installDir = $instance.installationPath
+    $msBuild = $installDir + '\MSBuild\Current\Bin\MSBuild.exe'
+    if (![System.IO.File]::Exists($msBuild))
+    {
+        $msBuild = $installDir + '\MSBuild\15.0\Bin\MSBuild.exe'
+        if (![System.IO.File]::Exists($msBuild))
+        {
+            Write-Host "MSBuild doesn't exist. Exit."
+            exit 1
+        }
+        else
+        {
+            Write-Host "Likely on Windows with VS2017."
+        }
+    }
+    else
+    {
+        Write-Host "Likely on Windows with VS2019."
+    }
 }
 
 Write-Host "MSBuild found. Compile the projects."
