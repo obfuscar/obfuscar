add6734e61554149e61de0a358d6492bf5c1fd31
Lex Li <support@lextm.com>
2021-11-17 21:01:22 -0800

    Fixed build scripts.

--- dist.publish2nugetdotorg.bat
+++ dist.publish2nugetdotorg.bat
@@ -1,5 +1,2 @@
-mkdir .nuget
-cd .nuget
 nuget update /self
-for %%f in (..\*.nupkg) do nuget push %%f -Source https://www.nuget.org/api/v2/package
-cd ..
+for %%f in (.\*.nupkg) do nuget push %%f -Source https://www.nuget.org/api/v2/package
