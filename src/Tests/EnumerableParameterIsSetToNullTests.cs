511d3810425be77a0b078c134806b527d4e97cbc
Andrii Protskiv <aprotskiv@gmail.com>
2026-07-12 21:48:16 +0700

    added unit-test  to reproduce "System.MissingFieldException: Field not found: 'C.list' for generic collection parameter

---
+++ src/Tests/EnumerableParameterIsSetToNullTests.cs
@@ -0,0 +1,53 @@
+using Obfuscar;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ObfuscarTests
+{
+    public class EnumerableParameterIsSetToNullTests
+    {
+        [Fact]
+        public async Task CheckEnumerableParameterIsNotSetToNull()
+        {
+            string outputPath = TestHelper.OutputPath;
+            string xml = string.Format(
+                @"<?xml version='1.0'?>" +
+                @"<Obfuscator>" +
+                @"<Var name='InPath' value='{0}' />" +
+                @"<Var name='OutPath' value='{1}' />" +
+                @"<Var name='KeepPublicApi' value='false' />" +
+                @"<Var name='HidePrivateApi' value='true' />" +
+                @"<Module file='$(InPath){2}EnumerableParameterIsSetToNullTest.dll' />" +
+                @"</Obfuscator>", TestHelper.InputPath, outputPath, Path.DirectorySeparatorChar);
+
+            TestHelper.BuildAndObfuscate(
+                "EnumerableParameterIsSetToNullTest",
+                string.Empty,
+                xml,
+                languageVersion: Microsoft.CodeAnalysis.CSharp.LanguageVersion.CSharp7);
+
+            string outputAssemblyPath = Path.Combine(outputPath, "EnumerableParameterIsSetToNullTest.dll");
+            Assembly assembly = Assembly.LoadFile(outputAssemblyPath);
+
+            Type derivedType = assembly.GetType("EnumerableParameterIsSetToNullTest.EnumerableParameterIsSetToNullExample_ChildClass", throwOnError: true);
+
+            var instance = Activator.CreateInstance(derivedType);
+
+            MethodInfo method = derivedType.GetMethod("PublicMethodAsync");
+
+            bool? result = null;
+
+            // Act
+            var exception = await Record.ExceptionAsync( async () =>  {
+                result = await (Task<bool>)method.Invoke(instance, new object[] { });
+            });
+
+            // Assert
+            Assert.Null(exception);
+            Assert.True(result);
+        }
+    }
+}
---
+++ src/Tests/Input/EnumerableParameterIsSetToNullTest.cs
@@ -0,0 +1,95 @@
+// This example covers two bugs:
+// 1.  IEnumerable|List|ICollection parameter is not used in method.
+//     Instead new 'source' variable declared and is set as NULL:
+//          List<EnumerableParameterIsSetToNullExample_ItemClass> source = default(List<EnumerableParameterIsSetToNullExample_ItemClass>);
+// 2. System.MissingFieldException: Field not found: 'C.list'.
+//      But 'list' is parameter, not a field.
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace EnumerableParameterIsSetToNullTest
+{
+    [Obfuscation]
+    public interface IEnumerableParameterIsSetToNullExample_Interface
+    {
+        [Obfuscation]
+        Task<bool> PublicMethodAsync();
+    }
+
+    // [Obfuscation]
+    public abstract class EnumerableParameterIsSetToNullExample_AbstractClass : IEnumerableParameterIsSetToNullExample_Interface
+    {
+        [Obfuscation]
+        public async Task<bool> PublicMethodAsync()
+        {
+            var list = new List<EnumerableParameterIsSetToNullExample_ItemClass>();
+            list.Add(new EnumerableParameterIsSetToNullExample_ItemClass());
+
+            return
+                await _ListAsync(list)
+                &&
+                await _EnumerableAsync(list)
+                &&
+                await _CollectionAsync(list)
+                &&
+                await _ArrayAsync(list.ToArray());
+        }
+
+        [Obfuscation]
+        private async Task<bool> _ListAsync(List<EnumerableParameterIsSetToNullExample_ItemClass> list)
+        {
+            if (list.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [Obfuscation]
+        private async Task<bool> _EnumerableAsync(IEnumerable<EnumerableParameterIsSetToNullExample_ItemClass> enumerable)
+        {
+            if (enumerable.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [Obfuscation]
+        private async Task<bool> _CollectionAsync(ICollection<EnumerableParameterIsSetToNullExample_ItemClass> collection)
+        {
+            if (collection.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [Obfuscation]
+        private async Task<bool> _ArrayAsync(EnumerableParameterIsSetToNullExample_ItemClass[] array)
+        {
+            if (array.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    [Obfuscation]
+    public class EnumerableParameterIsSetToNullExample_ChildClass : EnumerableParameterIsSetToNullExample_AbstractClass
+    {
+    }
+
+    [Obfuscation]
+    public class EnumerableParameterIsSetToNullExample_ItemClass
+    {
+        public string SomeProperty { get; set; }
+    }
+}
